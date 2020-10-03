5e9fe3c4b9c7ef7488f414234532f66507044fc7
Scott <scott.carnett@dvt-inc.com>
2020-10-03 09:53:02 -0400

    packages

--- android/app/src/main/kotlin/io/dispatcher/app/dev/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.dispatcher.app.dev
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/io/dispatcher/app/prod/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.dispatcher.app.prod
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
